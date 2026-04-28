ff62227c77a0b36b70f8ccfc952e73d38ea5145c
sayan <dassayan29062005@gmail.com>
2026-04-28 22:55:28 +0530

    initial set up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express= require("express");
-
+const {users} = require("./data/users.json")
 const app=express();
 
 const port=8081;
@@ -10,6 +10,27 @@ app.get("/",(req,res)=>{
     res.status(200).json({
         message: "Home page"
     });
+})
+app.get('/users',(req,res)=>{
+    res.status(200).json({
+        success: true,
+        data: users
+    })
+})
+
+app.get('/users/:id',(req,res)=>{
+    const {id} = req.params;
+    const user = users.find((each)=>each.id===id);
+    if(!user){
+       return res.status(404).json({
+            success:false,
+            message: `user not found ${id}`
+        })
+    }
+    res.status(200).json({
+        success: true,
+        data: user
+    })
 })
 
 // app.all("*",(req,res)=>{
